55f5566cd31bae08a3a3a1a81503ed10acca5a2d
Fabricio Sousa <fabricio1234@gmail.com>
2021-02-28 00:33:57 -0300

    adding more about services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,15 +3,34 @@ import "./Services.css";
 import { Link } from "react-router-dom";
 import BotPatricia from "../BotPatricia/BotPatricia";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  Dropdown,
+  Button,
+  ButtonGroup,
+  SplitButton,
+  DropdownButton,
+} from "react-bootstrap";
 
 const Services = () => {
+  function option() {}
   return (
     <>
       <div className="Services__container">
-          <div className='nav-top'>
-            <span>Servicos</span>
-          </div>
+        <div className="nav-top">
+          <span>Serviços</span>
+          <DropdownButton
+          className='button-option'
+            menuAlign="right"
+            id="dropdown-menu-align-right"
+          >
+            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownButton>
+        </div>
       </div>
     </>
   );
