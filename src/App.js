7b194bb69a6edd4c76e69c6bdedb95bb11119c26
Fabricio de sousa <fabricio1234c@gmail.com>
2021-04-05 12:30:51 -0300

    adding (feat:meetboot)

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import Patricia from "./components/BotOne/BotOne";
 import Purpose from "./components/Pages/Purpose/Purpose";
 import Meetbot from "./components/Pages/Meetboots/Meetboots";
 
-import firebase from "firebase/app";
-import "firebase/firebase-firestore";
-import "firebase/firebase-auth";
 
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollectionData } from "react-firebase-hooks/firestore";
 
 import {
   BrowserRouter as Router,
@@ -23,19 +18,6 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import Home from "./components/Pages/Home/Home";
-
-firebase.initializeApp({
-  apiKey: "AIzaSyAuoRdexRNLcDw0E7n8Gdnr7ZRhFDJ05Nk",
-  authDomain: "chatbot-8c00d.firebaseapp.com",
-  projectId: "chatbot-8c00d",
-  storageBucket: "chatbot-8c00d.appspot.com",
-  messagingSenderId: "289050771352",
-  appId: "1:289050771352:web:f98061b6f861d36c7fb5ed",
-  measurementId: "G-VW9EX01R4P",
-});
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
 
 
 function App() {
